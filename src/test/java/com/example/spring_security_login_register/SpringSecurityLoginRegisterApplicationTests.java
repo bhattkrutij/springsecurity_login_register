c7e5fe4dac67a71f4d5002c1964a0f34300b41fe
DELL <krutibhatt.1231@gmail.com>
2023-10-03 11:33:25 +0530

    inital commit

---
+++ src/test/java/com/example/spring_security_login_register/SpringSecurityLoginRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_login_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
